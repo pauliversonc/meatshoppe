76ba3bdac8bf022ec34e71ace4736a63b755be83
Paul Iverson Cortez <pauliversonc@gmail.com>
2023-09-19 21:19:45 +0800

    Add search bar

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="header__wrapper"
-    :class="{ 'active': scrollY >= 240 }"
+    :class="{ active: scrollY >= 240, pr: isSearchVisible }"
     v-motion
     :initial="{
       x: 200,
@@ -19,7 +19,7 @@
       },
     }"
   >
-    <header class="header">
+    <header class="header" :class="{ pr: isSearchVisible }">
       <div class="header__nav">
         <a href="#" class="header__logo-link">
           <img
@@ -46,7 +46,16 @@
       </div>
 
       <div class="header__links" :class="{ pr: isActive }">
-        <div class="header__icon-link">
+        <div
+          class="header__icon-link"
+          @click="isSearchVisible = true"
+          role="button"
+        >
+          <svg class="header__icon">
+            <use xlink:href="../../assets/icons/sprite.svg#icon-search"></use>
+          </svg>
+        </div>
+        <div class="header__icon-link" role="button">
           <svg class="header__icon">
             <use
               xlink:href="../../assets/icons/sprite.svg#icon-shopping-cart"
@@ -68,17 +77,108 @@
       </div>
     </header>
   </div>
+
+  <div class="search" @click="closeSearch2" v-if="isSearchVisible">
+    <div class="search__container">
+      <el-form
+        class="search__input-wrapper"
+        :model.trim="search"
+        @submit.prevent="submitSearch"
+      >
+        <el-input
+          v-model="search.keyword"
+          size="large"
+          placeholder="Search"
+          clearable
+          @clear="clearSearch"
+          @keydown.down="selectNext"
+          @keydown.up="selectPrevious"
+          @input="clearSearch"
+        />
+      </el-form>
+
+      <div class="search__btn-wrapper">
+        <BaseButton @click="closeSearch" btnText="close" />
+      </div>
+
+      <ul class="search__suggestions">
+        <li
+          class="search__suggestion"
+          v-for="(item, index) in filteredSuggestions"
+          :key="index"
+        >
+          <a
+            href="#"
+            class="search__link"
+            :class="{ active: index === search.selectedSuggestion }"
+          >
+            <div class="search__icon-box" role="button">
+              <svg class="search__icon">
+                <use
+                  xlink:href="../../assets/icons/sprite.svg#icon-search"
+                ></use>
+              </svg>
+            </div>
+            <span>{{ item }}</span></a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MeatshoppeTheHeader",
-  computed: {},
+  computed: {
+    filteredSuggestions() {
+      // Filter suggestions based on the searchQuery
+      const filtered = this.search.suggestions.filter((suggestion) =>
+        suggestion.toLowerCase().includes(this.search.keyword.toLowerCase())
+      );
+
+      return filtered.slice(0, 10);
+    },
+  },
 
   data() {
     return {
       isActive: false,
       scrollY: 0,
+
+      isSearchVisible: false,
+      search: {
+        keyword: "",
+        activeSuggestion: "",
+        selectedSuggestion: -1,
+        suggestions: [
+          "pizza",
+          "sushi",
+          "burger",
+          "pasta",
+          "tacos",
+          "salad",
+          "fried chicken",
+          "ice cream",
+          "steak",
+          "shrimp scampi",
+          "dim sum",
+          "burrito",
+          "lobster",
+          "ramen",
+          "chocolate cake",
+          "biryani",
+          "gyros",
+          "pho",
+          "pancakes",
+          "falafel",
+          "caesar salad",
+          "hot dog",
+          "sashimi",
+          "chicken tikka masala",
+          "pad thai",
+        ],
+      },
     };
   },
 
@@ -86,14 +186,16 @@ export default {
     isActive(newValue) {
       if (newValue) {
         document.body.classList.add("show-menu");
-
-        // document.body.style.overflow = "hidden";
-        // document.body.style.paddingRight = "17px";
       } else {
         document.body.classList.remove("show-menu");
-        // document.body.style.overflow = "visible";
-        // document.body.style.overflowX = "hidden";
-        // document.body.style.paddingRight = "0";
+      }
+    },
+
+    isSearchVisible(newValue) {
+      if (newValue) {
+        document.body.classList.add("show-menu");
+      } else {
+        document.body.classList.remove("show-menu");
       }
     },
   },
@@ -118,6 +220,66 @@ export default {
       // Update the scrollY data property with the current scroll position
       this.scrollY = window.scrollY;
     },
+
+    submitSearch() {
+      // get the text of active suggestions
+      const element = document.querySelector(".search__link.active span");
+      const activeSuggestion = element?.innerHTML;
+      // if there is active suggestion set it as a keyword
+      if (activeSuggestion) {
+        this.search.keyword = activeSuggestion;
+        this.search.selectedSuggestion = 0;
+      }
+
+      console.log(this.search.keyword);
+    },
+
+    selectNext() {
+      // if products is true
+      if (this.search.suggestions.length > 0) {
+        if (
+          this.search.selectedSuggestion <
+          this.filteredSuggestions.length - 1
+        ) {
+          this.search.selectedSuggestion++;
+        } else {
+          this.search.selectedSuggestion = 0;
+        }
+      }
+    },
+
+    selectPrevious(event) {
+      event.preventDefault();
+      if (this.search.suggestions.length > 0) {
+        if (this.search.selectedSuggestion > 0) {
+          this.search.selectedSuggestion--;
+        } else {
+          this.search.selectedSuggestion = this.filteredSuggestions.length - 1;
+        }
+      }
+    },
+
+    closeSearch() {
+      this.isSearchVisible = false;
+      this.search.selectedSuggestion = -1;
+      this.search.keyword = "";
+    },
+
+    clearSearch() {
+      // This method is triggered whenever the user types in the input field
+      // It updates the list of suggestions based on the input
+      this.search.selectedSuggestion = -1;
+    },
+
+    closeSearch2(event) {
+      // get class of clicked element
+      const element = event.target.classList.value;
+
+      // check if the click is the outside of the search box
+      if (element !== "search") return;
+
+      this.closeSearch();
+    },
   },
 };
 </script>
@@ -133,6 +295,11 @@ export default {
   height: 100%;
   text-transform: uppercase;
 
+  &.pr {
+    // padding-right: 17px; // this is the old pr
+    padding-right: 9px;
+  }
+
   // border: 1px solid red;
   justify-content: space-between;
   align-items: center;
@@ -142,12 +309,12 @@ export default {
     padding: 0 2rem;
     height: 7rem;
     width: 100%;
-    z-index: 9999;
-    
-    transition: background-color .4s ease;
+    z-index: 10;
+    // background-color: rgba($color: $light-mid, $alpha: 0.9);
+    transition: background-color 0.4s ease;
 
     backdrop-filter: blur(10px); /* Adjust the blur amount as needed */
-  
+
     &.active {
       background-color: rgba($color: #ffffff, $alpha: 0.9);
     }
@@ -304,6 +471,91 @@ export default {
   &__icon {
     height: 2.7rem;
     width: 100%;
+  }
+}
+
+.search {
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  z-index: 12;
+  background-color: rgba($dark-high, 0.5);
+  padding-top: 8rem; // web view
+  padding-right: 17px;
+
+  &__container {
+    padding: 1rem;
+    margin: 0 auto;
+    // max-width: 120rem; // mobile view
+    max-width: 80rem; // web view
+
+    padding: 2rem;
+    // height: 20rem;
+    // border: 1px solid red;
+    background-color: $light-mid;
+    // background-color: red;
+    box-shadow: $shadow;
+
+    display: grid;
+    grid-template-columns: 1fr auto;
+    // justify-items: center;
+    align-items: center;
+
+    gap: 1rem;
+  }
+
+  &__input-wrapper {
+    width: 100%;
+  }
+
+  &__suggestions {
+    grid-column: 1/-1;
+    // background-color: blue;
+    // display: none;
+    // padding: 1rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.6rem;
+  }
+  &__suggestion {
+    list-style: none;
+  }
+
+  &__link {
+    text-decoration: none;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 0.6rem 1rem;
+    color: $black-tint;
+    background-color: $light-high;
+
+    box-shadow: $shadow;
+
+    &:hover,
+    &.active {
+      background-color: $main;
+      color: $white-tint;
+
+      box-shadow: $shadow-hov;
+      .search__icon {
+        fill: $white;
+      }
+    }
+  }
+
+  &__icon-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &__icon {
+    height: 1.8rem;
+    width: 1.8rem;
+    fill: $black;
   }
 }
 </style>
