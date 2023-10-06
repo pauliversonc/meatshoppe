c4ab1d2b8c24abbb58c07fa73f27cf5887ca1464
Paul Iverson Cortez <pauliversonc@gmail.com>
2023-10-06 11:45:12 +0800

    add product page and route

---
+++ src/components/pages/ProductPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    pusy
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeatshoppeProductPage',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/router/routes/productRoute.js
@@ -0,0 +1,7 @@
+import ProductPage from '../../components/pages/ProductPage.vue';
+
+const productRoute = {
+  path: '/product', component: ProductPage
+}
+
+export default productRoute;
